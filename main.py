fddfb2c76da1aed6601d3def0ce0ef4081635e8a
gks-spa <gks-spa@users.noreply.github.com>
2020-03-28 16:19:39 -0500

    Implement rref and some properties; add comments

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+# LinearAlgebraPython, by Gabriel Konar-Steenberg
+# An attempt to write out in code some linear algebra algorithms and properties, mostly for my own educational benefit
+# Technical correctness is a high priority; optimization is very much not. Elegance is nice.
+# I use the built-in Python libraries, but no external libraries -- in particular, no NumPy.
+# From time to time I cite my first and second linear algebra textbooks with an abbreviation and a section or page number; these textbooks are, respectively:
+#   B:  Bretscher, Otto (2013). "Linear Algebra with Applications (Fifth Edition)."
+#   GH: Garcia, Stephan Ramon and Horn, Roger A. (2017). "A Second Course in Linear Algebra."
+# Conventions:
+#   - I always write "col" instead of "column"
+#   - Everything is indexed from 0, so the top-left entry of a matrix is mat[0,0]
+#   - The 0x0 matrix exists and its representation in array form is [] (not [[]])
+# Major TODO items:
+#   - TODO: Handle fractions nicely using Python's native fraction support
+#   - TODO: Implement automatic fraction handling to avoid annoying floating point errors (2.999999...8)
+#   - TODO: Split the Matrix class off into a matrix.py and just put tests here
+
 import time
 import numbers
 import copy
+import decimal
+from random import random
 
 def main():
     # Real vectors (are matrices)
@@ -50,31 +68,102 @@ def main():
     # print(m+n)
     # print(m-n)
     # print(5*m)
+    # print(m*5)
     # print(m*n)
+    # print(m/4)
 
-def empty1d(length):
-    return [None] * length
+    #Enumeration
+    # for (i,j),x in a.enumerate():
+        # print("["+str(i)+","+str(j)+"]: "+str(x))
 
-def empty2d(rows, cols):
-    return [empty1d(cols) for i in range(rows)]
+    #Identity matrix
+    # print(Matrix.ident(5))
 
-Scalar = numbers.Complex
+    #Equality
+    # print(Matrix([[5]]) == Matrix([[5]]))
+    # print(Matrix([[5, 6]]) == Matrix([[5]]))
+    # print(Matrix([[6]]) == Matrix([[5]]))
 
-class Matrix():
+    #Block matrices
+    block = Matrix.from_block([                      # [1 1|2]
+        [Matrix([[1,1],[1,1]]), Matrix([[2],[2]])],  # [1_1|2]
+        [Matrix([[3,3]]), 4]                         # [3 3|4]
+    ])
+    # print(block)
+
+    #Sorting of rows or cols
+    # print(Matrix([[1,2,3],[4,5,6],[7,8,9]]).sort(lambda r: random(), dim="rows"))
+
+    #rref and row operations
+    # print(a.rref())
+    r = Matrix.construct_by_indices(lambda i,j: random(), 5, 6)
+    # print(r)
+    # print(r.rref())
+    g = Matrix([
+        [1, 2, 0, 0, 3],
+        [0, 0, 0, 1, 1],
+        [0, 0, 3, 0, 9],
+        [0, 0, 0, 0, 0]
+    ])
+    g[0] += 2*g[1]
+    g[1] += 2*g[0]
+    g[2] += 2*g[0]
+    g[3] += 2*g[2] -2*g[1] +4*g[0]
+    # print(g)
+    # print(g.rref())
+
+
+Scalar = numbers.Complex  # GH 0.2
+
+def kronecker_delta(i, j):  # GH pg3
+    return 1 if i == j else 0
+
+def is_2d_non_jagged_of_type(arr, type):
+    if arr == [[]]: return False  # We adopt the convention that a 0x0 2d matrix is [], not [[]]
+    if arr == []: return True
+    for row in arr:
+        if len(row) != len(arr[0]): return False  # Ensures that the array is not jagged
+        for value in row:
+            if not isinstance(value, type): return False  # Ensures that all elements are numbers
+    return True
+
+class Matrix():  # GH 0.3
     # CREATION AND MUTATION
     def __init__(self, values):
-        assert values != [[]]  # There shall be only one representation of a 0x0 matrix: values == []
-        self.__rows = len(values)
-        self.__cols = len(values[0]) if self.rows > 0 else 0
-        for row in values:
-            assert len(row) == self.cols  # Ensures that the array is not jagged
-            for value in row:
-                assert isinstance(value, Scalar)  # Ensures that all elements are numbers
-        self.values = copy.deepcopy(values)
+        self.reset(values)
+    
+    def reset(self, values):  # Re-initialize
+        if isinstance(values, Scalar): self.reset([[values]])  # Tolerate scalars by converting them to 1x1 matrices
+        else:
+            assert is_2d_non_jagged_of_type(values, Scalar)
+            self.values = copy.deepcopy(values)
 
-    @staticmethod
-    def vector_from_list(values):
-        return Matrix([values]).transpose()
+    def assign(self, other):  # Mutate self into other (i.e., make self equal to other without messing with references)
+        self.reset(other.values)
+    
+    @classmethod
+    def from_block(cls, submatrices):  # Assumes that the blocks line up, i.e., all the blocks on a given input row (resp. col) have the same number of rows (resp. cols)
+        for submatrices_row in range(len(submatrices)):
+            for submatrices_col in range(len(submatrices[submatrices_row])):
+                if isinstance(submatrices[submatrices_row][submatrices_col], Scalar):
+                    submatrices[submatrices_row][submatrices_col] = Matrix(submatrices[submatrices_row][submatrices_col])  # Tolerate scalars by converting them to 1x1 matrices
+
+        assert is_2d_non_jagged_of_type(submatrices, cls)
+        submatrices_rows = len(submatrices)
+        submatrices_cols = len(submatrices[0]) if submatrices_rows > 0 else 0
+        result = cls.zero(sum([submatrices[i][0].rows for i in range(submatrices_rows)]), sum([submatrices[0][j].cols for j in range(submatrices_cols)]))
+        for submatrices_row in range(len(submatrices)):
+            for submatrices_col in range(len(submatrices[submatrices_row])):
+                submatrix = submatrices[submatrices_row][submatrices_col]
+                result_row_offset = sum([submatrices[i][0].rows for i in range(submatrices_row)])
+                result_col_offset = sum([submatrices[0][j].cols for j in range(submatrices_col)])
+                for (i,j),x in submatrix.enumerate():
+                    result[result_row_offset+i, result_col_offset+j] = submatrix[i,j]
+        return result
+
+    @classmethod
+    def vector_from_list(cls, values):
+        return cls([values]).transpose()
     
     def vector_to_list(self):
         assert self.is_vector()
@@ -82,21 +171,44 @@ class Matrix():
     
     def is_vector(self):
         return self.cols == 1
+    
+    def unpack_index_key(self, key): # Basically i,j=key with lots of error handling and special cases
+        try:
+            i,j = key
+            col_present = True
+        except TypeError:
+            i,j = (key, slice(None, None, None))  # Shortcut for rows: you can do mat[3] instead of mat[3,:]
+            col_present = False
+
+        row_malformed = not(isinstance(i, int) or isinstance(i, slice))
+        col_malformed = not(isinstance(j, int) or isinstance(j, slice))
+        if row_malformed or col_malformed:
+            if row_malformed and not col_present: msg = "Malformed key: key is "+str(type(key))+"; should be int|slice or (int|slice, int|slice) tuple"
+
+            elif row_malformed: msg = "Malformed row index: row index is "+str(type(i))+"; should be int|slice."
+            else: msg = "Row OK."
+
+            if col_malformed: msg += " Malformed col index: col index is "+str(type(i))+"; should be int|slice."
+            else: msg = "Col OK."
+
+            raise TypeError(msg)
+        
+        return i,j
 
     def __getitem__(self, key):
-        i,j = key
+        i,j = self.unpack_index_key(key)
         if isinstance(i, slice) or isinstance(j, slice):
             sliced_rows = self.values[i]
             if len(sliced_rows) == 0: return []
             if type(sliced_rows[0]) is not list: sliced_rows = [sliced_rows]
             result = Matrix([(row[j] if type(row[j]) is list else [row[j]]) for row in sliced_rows])
-            if result.rows == 1 and result.cols == 1: return result[0][0]
+            if result.rows == 1 and result.cols == 1: return result[0,0]  # If the result is a single value, return the value instead of a 1x1 matrix
             else: return result
         else:
             return self.values[i][j]
     
     def __setitem__(self, key, value):
-        i,j = key
+        i,j = self.unpack_index_key(key)
         if isinstance(value, Matrix): value = value.values
         if isinstance(i, slice) or isinstance(j, slice):
             sliced_rows = self.values[i]
@@ -112,11 +224,11 @@ class Matrix():
     #PROPERTIES
     @property
     def rows(self):
-        return self.__rows
+        return len(self.values)
     
     @property
     def cols(self):
-        return self.__cols
+        return len(self.values[0]) if self.rows > 0 else 0
     
     @property
     def length(self):
@@ -131,7 +243,8 @@ class Matrix():
     def __str__(self):
         return "["+" \n ".join(["["+", ".join([str(v) for v in row])+"]" for row in self.values])+"]"
     
-    #ELEMENT-WISE OPERATIONS
+    #ELEMENT MANIPULATION
+    # TODO: come up with a more generalized way of doing inplace
     def __iter__(self):
         return self.MatrixIterator(self)
     
@@ -149,7 +262,36 @@ class Matrix():
                 self.col = 0
                 self.row += 1
             return result
+    
+    def enumerate(self):  # Iterate through the rows in the order (1, 1), (1, 2), ..., (1, cols-1), (2, 1), (2, 2), ..., (rows-1, cols-1) and return ((row, col), value) at each point
+        for i in range(self.rows):
+            for j in range(self.cols):
+                yield ((i, j), self[i,j])
+    
+    def enumrows(self):
+        for i in range(self.rows):
+            yield (i, self[i,:])
         
+    def enumcols(self):
+        for j in range(self.cols):
+            yield (j, self[:,j])
+        
+    def sortrows(self, key, inplace=False):
+        return self.sort(key, "rows", inplace)
+    
+    def sortcols(self, key, inplace=False):
+        return self.sort(key, "cols", inplace)
+    
+    def sort(self, key, dim, inplace=False):  # Sort the rows or cols of a matrix by key, where key is a function that can be applied to a row or col matrix to get something sortable
+        if inplace:
+            self.assign(self.sort(key, dim, inplace=False))
+        else:
+            assert dim == "rows" or dim == "cols"
+            l = [r for _,r in self.enumrows()] if dim == "rows" else [c for _,c in self.enumcols()]
+            l = sorted(l, key=key)
+            return Matrix.from_block([[r] for r in l]) if dim == "rows" else Matrix.from_block([l])
+
+    #TODO: maybe implement a swap method for row/col swapping
 
     def apply(self, f, inplace=False):
         if not inplace: 
@@ -157,33 +299,38 @@ class Matrix():
             result.apply(f, inplace=True)
             return result
         else:
-            for i in range(self.rows):
-                for j in range(self.cols):
-                    self[i,j] = f(self[i,j])
+            for (i,j),x in self.enumerate():
+                    self[i,j] = f(x)
     
     def apply_binary(self, other, f, inplace=False):
-        if not inplace: 
+        if not inplace:
             result = self.copy()
             result.apply_binary(other, f, inplace=True)
             return result
         else:
-            for i in range(self.rows):
-                for j in range(self.cols):
-                    self.values[i][j] = f(self.values[i][j], other.values[i][j])
+            for (i,j),x in self.enumerate():
+                self[i,j] = f(x, other[i,j])
+        
+    
+    @classmethod
+    def construct_by_indices(cls, f, rows, cols):  # Given a function f that takes a set of row,col indices and returns an entry, this method creates a matrix of the desired size
+        result = cls.zero(rows, cols)
+        for (i,j),_ in result.enumerate():
+            result[i,j] = f(i,j)
+        return result
     
     #ELEMENTARY UNARY OPERATIONS
     def __pos__(self): return self.apply(lambda x: +x)
     def __neg__(self): return self.apply(lambda x: -x)
-    def __abs__(self): return self.apply(lambda x: abs(x))
+    # def __abs__(self): return self.apply(lambda x: abs(x))
 
     def transpose(self):
-        result = empty2d(self.cols, self.rows)
-        for i in range(self.rows):
-            for j in range(self.cols):
-                result[j][i] = self.values[i][j]
-        return Matrix(result)
+        result = self.zero(self.cols, self.rows)
+        for (i,j),x in self.enumerate():
+                result[j,i] = x
+        return result
     
-    def det(self):
+    def det(self):  # TODO rewrite so it is easier to follow and doesn't refer to self.values
         assert self.rows == self.cols
         if self.rows == 0:
             return 1
@@ -191,11 +338,13 @@ class Matrix():
         for i in range(self.rows):
             sub = Matrix([[e for c, e in enumerate(row) if c != 0] for r, row in enumerate(self.values) if r != i])
             d = sub.det()
-            x = ((-1)**i)*self.values[i][0]*d
+            x = ((-1)**i)*self[i,0]*d
             result += x
         return result
 
     #ELEMENTARY BINARY OPERATIONS
+    def __eq__(self, other): return self.rows == other.rows and self.cols == other.cols and all([x == other[i,j] for (i,j),x in self.enumerate()])
+
     def __add__(self, other): return self.apply_binary(other, lambda x,y: x+y)
     def __sub__(self, other): return self.apply_binary(other, lambda x,y: x-y)
 
@@ -209,19 +358,63 @@ class Matrix():
             return self.apply(lambda x: other*x)
         else:
             assert self.cols == other.rows
-            result = empty2d(other.cols, self.rows)
-            for i in range(self.rows):
-                for j in range(other.cols):
-                    result[i][j] = self[i,:].transpose().dot(other[:,j])
-            return Matrix(result)
+            result = self.zero(other.cols, self.rows)
+            for (i,j),_ in self.enumerate():
+                    result[i,j] = self[i,:].transpose().dot(other[:,j])
+            return result
 
     def __rmul__(self, other):
         assert isinstance(other, Scalar)
         return self*other
     
-    def __div__(self, other):
+    def __truediv__(self, other):
         assert isinstance(other, Scalar)
         return self.apply(lambda x: x/other)
+
+    #SPECIAL MATRICES
+    @classmethod
+    def zero(cls, rows, cols):
+        return cls([[0] * cols for i in range(rows)])
+    
+    @classmethod
+    def ident(cls, n):  # GH pg3
+        return cls.construct_by_indices(kronecker_delta, n, n)
+    
+    #TESTS FOR PROPERTIES
+    def is_zero(self):
+        return all([x == 0 for x in self])
+
+    def is_identity(self):
+        return all([x == kronecker_delta(i,j) for (i,j),x in self.enumerate()])
+
+    def is_inverse(self, other):
+        return (self*other).is_identity()
+    
+    #ALGORITHMS
+    def rref(self):  # B pg15
+        result = self.copy()
+        for i,row in result.enumrows():
+            # Find the leading value in row i
+            (_,leading_index),leading_value = row.leading()
+
+            # Normalize the leading value, if possible
+            if leading_index is None: break
+            else: result[i,:] /= leading_value
+
+            # Eliminate the rest of the values in col leading_index
+            for i_e, row_e in result.enumrows():
+                if i_e != i:
+                    result[i_e,:] -= result[i,:]*row_e[0,leading_index]
+        
+        # Sort the rows so that leading values go from left to right as row number increases
+        # We do this with a nested lambda expression that returns the index of the leading value if it exists, otherwise the number of cols in the row. Nesting allows us to only call leading() once per row.
+        return result.sortrows(lambda row: (lambda row, leading: leading if leading is not None else row.cols)(row, row.leading()[0][1]))
+    
+    def leading(self):  # Returns (row, col), value for the leading value. This is the leading (i.e., first nonzero) value in the first nonzero row.
+        for (i,j),x in self.enumerate():  # enumerate() is guaranteed to iterate the way we want it to
+            if x != 0:
+                return ((i,j),x)
+        return ((None,None),None)
 
 if __name__ == "__main__":
     main()
